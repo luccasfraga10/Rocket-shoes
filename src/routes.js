b720c914c2b9e310696f6f242bb8bd6220cf1e64
Luccas Fraga <luccas.fraga@ifood.com.br>
2020-03-04 23:30:13 -0300

    Fix path route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import { Route, Switch } from 'react-router-dom';
 
 import React from 'react';
-import Cart from './Pages/Cart';
-import Home from './Pages/Home';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
 
 export default function Routes() {
   return (
